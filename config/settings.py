637aad7677391348a733da24697c8943799438b0
unknown <shatytshipala@gmail.com>
2024-04-17 04:38:26 -0700

    HUFEDILE6

--- config/settings.py
+++ config/settings.py
@@ -40,10 +40,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env("SECRET_KEY2")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
