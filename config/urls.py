393e99d794bb3d1d2e05dbd270bfc2d212590e79
unknown <shatytshipala@gmail.com>
2024-04-20 10:08:56 -0700

    SY

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import handler404, handler500, handler400
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from . import views
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
