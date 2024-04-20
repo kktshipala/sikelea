12449148db9587aa6c8233ef3427d65af70eec89
unknown <shatytshipala@gmail.com>
2024-04-20 10:18:18 -0700

    SYY

--- config/settings.py
+++ config/settings.py
@@ -211,7 +211,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "SIKELEA/static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
