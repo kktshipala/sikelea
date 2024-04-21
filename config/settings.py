c1deae600caec2a16673beb8d37302e71446e22c
unknown <shatytshipala@gmail.com>
2024-04-21 10:02:21 -0700

    aaa

--- config/settings.py
+++ config/settings.py
@@ -213,16 +213,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
     
 ]
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["staticfiles"]))
+#STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["staticfiles"]))
 
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # -----------------------------------
 # E-mail configuration
