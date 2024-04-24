0631f0f097ac630e45fa2d849f7ba13d05cde775
unknown <shatytshipala@gmail.com>
2024-04-24 03:31:05 -0700

    STATIC

--- config/settings.py
+++ config/settings.py
@@ -223,7 +223,7 @@ STATICFILES_DIRS = [
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["staticfiles"]))
