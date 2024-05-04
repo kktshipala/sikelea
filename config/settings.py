a2e03893a92456d74be27ee8f35e608f31848667
unknown <shatytshipala@gmail.com>
2024-05-04 03:17:54 -0700

    rrr

--- config/settings.py
+++ config/settings.py
@@ -216,19 +216,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "SIKELEA/static/"
-MEDIA_URL = "SIKELEA/media/"
+STATIC_URL = "static/"
+MEDIA_URL = "media/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "SIKELEA/static"),
+    os.path.join(BASE_DIR, "static"),
     ]
 
 
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "SIKELEA/static/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "SIKELEA/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 
