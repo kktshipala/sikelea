06803831ee9a33245f73c86ec8712038ad83b47b
unknown <shatytshipala@gmail.com>
2024-05-04 02:35:08 -0700

    //oo

--- config/settings.py
+++ config/settings.py
@@ -223,22 +223,18 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-   
-
-    
-]
+    os.path.join(BASE_DIR, "static"),]
 
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static")
+#MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-
-#STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["staticfiles"]))
+MEDIA_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["static"]))
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ["staticfiles"]))
 
 # -----------------------------------
 # E-mail configuration
