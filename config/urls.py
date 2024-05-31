4727c15c243eca7e9a3961adccb93bc7808dc90a
unknown <shatytshipala@gmail.com>
2024-05-31 10:37:12 -0700

    humanitarian9

--- config/urls.py
+++ config/urls.py
@@ -26,11 +26,11 @@ urlpatterns = [
     path("accounts/api/", include("accounts.api.urls", namespace="accounts-api")),
     path("admin/", admin.site.urls),
   #  path("",include("user_sessions.urls", "user_sessions")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-#if settings.DEBUG:
-    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     #urlpatterns.append(url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
 
 # handler404 = 'app.views.handler404'
