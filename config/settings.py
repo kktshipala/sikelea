aa0ee2bc778783144c43fd6ca2ebb7be521df848
unknown <shatytshipala@gmail.com>
2024-06-07 10:22:44 -0700

    fixed typo

--- config/settings.py
+++ config/settings.py
@@ -218,12 +218,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
